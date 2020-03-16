0440cda16ab95bf1b487cee51db314a7187aacbd
Herve Desrosiers <hdesrosiers@stingray.com>
2020-03-16 07:33:23 -0400

    added guidelines and galaxy icon size animations

--- app/src/main/java/com/raywenderlich/android/razegalactic/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/razegalactic/MainActivity.kt
@@ -30,10 +30,14 @@
 
 package com.raywenderlich.android.razegalactic
 
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import android.support.constraint.ConstraintSet
 import android.support.v7.app.AppCompatActivity
+import android.transition.AutoTransition
 import android.transition.TransitionManager
+import android.view.animation.LinearInterpolator
 import kotlinx.android.synthetic.main.keyframe1.*
 
 /**
@@ -53,15 +57,46 @@ class MainActivity : AppCompatActivity() {
     constraintSet1.clone(constraintLayout) //1
     constraintSet2.clone(this, R.layout.activity_main) //2
 
-    departButton.setOnClickListener { //3
-      //apply the transition
-      TransitionManager.beginDelayedTransition(constraintLayout) //4
-      val constraint = if (!isOffscreen) constraintSet1 else constraintSet2
-      isOffscreen = !isOffscreen
-      constraint.applyTo(constraintLayout) //5
+    departButton.setOnClickListener {
+      //1
+      val layoutParams = rocketIcon.layoutParams as ConstraintLayout.LayoutParams
+      val startAngle = layoutParams.circleAngle
+      val endAngle = startAngle + (if (switch1.isChecked) 360 else 180)
+
+      //2
+      val anim = ValueAnimator.ofFloat(startAngle, endAngle)
+      anim.addUpdateListener { valueAnimator ->
+
+        //3
+        val animatedValue = valueAnimator.animatedValue as Float
+        val layoutParams = rocketIcon.layoutParams as ConstraintLayout.LayoutParams
+        layoutParams.circleAngle = animatedValue
+        rocketIcon.layoutParams = layoutParams
+
+        //4
+        rocketIcon.rotation = (animatedValue % 360 - 270)
+      }
+      //5
+      anim.duration = if (switch1.isChecked) 2000 else 1000
+
+      //6
+      anim.interpolator = LinearInterpolator()
+      anim.start()
     }
 
+  }
 
+  override fun onEnterAnimationComplete() { //1
+    super.onEnterAnimationComplete()
+
+    constraintSet2.clone(this, R.layout.activity_main) //2
+
+    //apply the transition
+    val transition = AutoTransition() //3
+    transition.duration = 1000 //4
+    TransitionManager.beginDelayedTransition(constraintLayout, transition) //5
+
+    constraintSet2.applyTo(constraintLayout) //6
   }
 
 //  The first two properties are the constraint sets that you’ll use to animate your view.
